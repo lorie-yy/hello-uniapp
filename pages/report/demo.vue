e2236c3fc44b5f6b04b82f0c6ddde79707a2ffdf
Imagine-Lorie <313966183@qq.com>
2019-04-18 14:34:48 +0800

    echarts功能

--- pages/report/demo.vue
+++ pages/report/demo.vue
@@ -1,7 +1,7 @@
 <template>
 	<view class="container">
 		<view class="canvasView">
-			<view class="title">雷达图示例</view>
+			<!-- <view class="title">雷达图示例</view> -->
 			<mpvue-echarts class="ec-canvas" @onInit="lineInit" canvasId="line" ref="lineChart" />
 		</view>
 	</view>
@@ -12,20 +12,30 @@
 	import mpvueEcharts from '@/components/mpvue-echarts/src/echarts.vue';
 
 	let lineOption = {
-    // title: {
-		// 	text: '基础雷达图'
-    // },
-    // tooltip: {},
+    title: {
+			text: '雷达图title',
+			// 标题的位置，此时放在图的底边
+			left: 'center',
+			top: 'bottom',
+			textStyle: {
+				color: 'rgba(221,221,221,1)', //标题颜色
+				fontSize: 14,
+				lineHeight: 20,
+			},
+    },
+		// tooltip: {},
+		// 图例
     // legend: {
 		// 	data: ['预算分配']
     // },
     radar: {
 			// shape: 'circle',
-			radius: 90,
-			center: ['50%', '20%'],
-			nameGap: 10,
+			radius: 90, // 雷达图整体大小比例
+			// center: ['50%', '50%'], // 雷达图整体位置
+			// splitNumber: 5, // 雷达图圈数设置
+			nameGap: 10, // 雷达图与name的统一距离
 			name: {
-				// formatter:'【{value}】',
+				// 底图-各个方向的name样式
 				formatter: function (value, indicator) {
 					console.log('name___value, indicator',value, indicator)
 					if(value==='好奇心') {
@@ -71,23 +81,48 @@
 					},
 				}
 			},
+			// 底图-添加展示刻度（4.X版本不支持）
+			// axisTick:{ 
+			// 	show: true,
+			// },
+			// axisLabel: {
+			// 	show: true,
+			// 	formatter: '{value}',
+			// 	textStyle: {
+			// 		color: 'black'
+			// 	}
+			// },
+			// 底图-各个方向指示（名称，最大值，雷达图与name的距离，最小值，颜色）
 			indicator: [
-				{ name: '好奇心', max: 50},
+				{ 
+					name: '好奇心', max: 50,
+					//若将此属性放在radar下，则每条indicator都会显示圈上的数值，放在这儿，只在此条indicator上显示（4.X版本不支持）
+					// axisLabel: {
+					// 	show: true,
+					// 	fontSize: 12,
+					// 	color: '#838D9E',
+					// 	showMaxLabel: true, //显示最大值，即外圈显示数字50
+					// 	showMinLabel: true, //显示最小值，即中心点显示0
+					// },
+				},
 				{ name: '创造性', max: 50, nameGap: -2},
-				{ name: '社交智慧和情商', max: 50},
-				{ name: '洞察力', max: 50},
+				{ name: '社交智慧和情商', max: 50, nameGap: -8},
+				{ name: '洞察力', max: 50, nameGap: -5},
 				{ name: '幽默和顽皮', max: 50, nameGap: -2}
 			],
+			// 底图-区域划分样式
 			splitArea: {
 				areaStyle: {
 					color: ['transparent', 'transparent', 'transparent', 'transparent', 'transparent'],
 				}
 			},
+			// 底图-坐标线样式
 			axisLine: {
 				lineStyle: {
 					// color: '#e6e6e6'
 				}
 			},
+			// 底图-分割线样式
 			splitLine: {
 				lineStyle: {
 					// color: '#e6e6e6'
@@ -97,10 +132,13 @@
 		series: [{
 			name: '预算 vs 开销（Budget vs spending）',
 			type: 'radar',
+			symbol: 'circle', // 拐点的样式，还可以取值'rect','angle'等
+			symbolSize: 10, // 拐点的大小
 			data : [
 				{
 					value : [40, 30, 35, 22, 19],
 					name : '预算分配',
+					// 数据-在拐点处显示数值
 					label: {
 						normal: {
 							show: true,
@@ -111,17 +149,21 @@
 							}
 						}
 					},
+					// 数据-区域边框和区域的颜色（以及拐点marker样式）
 					itemStyle: {
 						normal: {
-							color: '#37aab7'
+							color: '#37aab7',
+							borderColor: 'white', // 拐点的描边颜色。[ default: '#000' ]
+							borderWidth: 2, // 拐点的描边宽度，默认不描边。[ default: 0 ]
 						}
 					},
-
+					// 数据-线条样式
 					lineStyle: {
 						normal: {
 							color: '#37aab7',
 						}
 					},
+					// 数据-区域样式
 					areaStyle: {
 						normal: {
 							opacity: 0.1,
@@ -165,21 +207,11 @@
 	page,
 	view {
 		display: flex;
-		/* uni-app默认使用flex布局。因为flex布局有利于跨更多平台，尤其是采用原生渲染的平台。如不了解flex布局，请参考http://www.w3.org/TR/css3-flexbox/。若不需要flex布局可删除本行*/
 	}
 
-	page {
+	/* page {
 		min-height: 100%;
-	}
-
-	.page-section-title {
-		padding: 0 30upx;
-	}
-
-	.title {
-		margin-left: 30upx;
-		color: #8f8f94;
-	}
+	} */
 
 	.container {
 		padding-bottom: 30upx;
@@ -194,17 +226,8 @@
 
 	.ec-canvas {
 		width: 100%;
-		height: 100%;
-		flex: 1;
-	}
-
-	.canvas-bar {
-		display: flex;
-		flex-direction: row;
-		justify-content: space-between;
-	}
-
-	.update-btn {
-		margin-right: 30upx;
+		height: 260px;
+		/* 设置后会画布占据整个屏高 */
+		/* flex: 1; */
 	}
 </style>
--- unpackage/dist/dev/mp-weixin/pages/report/demo.js
+++ unpackage/dist/dev/mp-weixin/pages/report/demo.js
@@ -117,15 +117,25 @@ var echarts = _interopRequireWildcard(__webpack_require__(/*! @/components/echar
 //
 //
 //
-var mpvueEcharts = function mpvueEcharts() {return Promise.all(/*! import() | components/mpvue-echarts/src/echarts */[__webpack_require__.e("common/vendor"), __webpack_require__.e("components/mpvue-echarts/src/echarts")]).then(__webpack_require__.bind(null, /*! @/components/mpvue-echarts/src/echarts.vue */ "../../../../work/hello-uniapp/components/mpvue-echarts/src/echarts.vue"));};var lineOption = { // title: {
-  // 	text: '基础雷达图'
-  // },
+var mpvueEcharts = function mpvueEcharts() {return Promise.all(/*! import() | components/mpvue-echarts/src/echarts */[__webpack_require__.e("common/vendor"), __webpack_require__.e("components/mpvue-echarts/src/echarts")]).then(__webpack_require__.bind(null, /*! @/components/mpvue-echarts/src/echarts.vue */ "../../../../work/hello-uniapp/components/mpvue-echarts/src/echarts.vue"));};var lineOption = { title: { text: '雷达图title', // 标题的位置，此时放在图的底边
+    left: 'center', top: 'bottom', textStyle: { color: 'rgba(221,221,221,1)', //标题颜色
+      fontSize: 14,
+      lineHeight: 20 } },
+
+
   // tooltip: {},
+  // 图例
   // legend: {
   // 	data: ['预算分配']
   // },
-  radar: { // shape: 'circle',
-    radius: 90, center: ['50%', '20%'], nameGap: 10, name: { // formatter:'【{value}】',
+  radar: {
+    // shape: 'circle',
+    radius: 90, // 雷达图整体大小比例
+    // center: ['50%', '50%'], // 雷达图整体位置
+    // splitNumber: 5, // 雷达图圈数设置
+    nameGap: 10, // 雷达图与name的统一距离
+    name: {
+      // 底图-各个方向的name样式
       formatter: function formatter(value, indicator) {
         console.log('name___value, indicator', value, indicator);
         if (value === '好奇心') {
@@ -171,23 +181,48 @@ var mpvueEcharts = function mpvueEcharts() {return Promise.all(/*! import() | co
 
 
 
+    // 底图-添加展示刻度（4.X版本不支持）
+    // axisTick:{ 
+    // 	show: true,
+    // },
+    // axisLabel: {
+    // 	show: true,
+    // 	formatter: '{value}',
+    // 	textStyle: {
+    // 		color: 'black'
+    // 	}
+    // },
+    // 底图-各个方向指示（名称，最大值，雷达图与name的距离，最小值，颜色）
     indicator: [
-    { name: '好奇心', max: 50 },
+    {
+      name: '好奇心', max: 50
+      //若将此属性放在radar下，则每条indicator都会显示圈上的数值，放在这儿，只在此条indicator上显示（4.X版本不支持）
+      // axisLabel: {
+      // 	show: true,
+      // 	fontSize: 12,
+      // 	color: '#838D9E',
+      // 	showMaxLabel: true, //显示最大值，即外圈显示数字50
+      // 	showMinLabel: true, //显示最小值，即中心点显示0
+      // },
+    },
     { name: '创造性', max: 50, nameGap: -2 },
-    { name: '社交智慧和情商', max: 50 },
-    { name: '洞察力', max: 50 },
+    { name: '社交智慧和情商', max: 50, nameGap: -8 },
+    { name: '洞察力', max: 50, nameGap: -5 },
     { name: '幽默和顽皮', max: 50, nameGap: -2 }],
 
+    // 底图-区域划分样式
     splitArea: {
       areaStyle: {
         color: ['transparent', 'transparent', 'transparent', 'transparent', 'transparent'] } },
 
 
+    // 底图-坐标线样式
     axisLine: {
       lineStyle: {
         // color: '#e6e6e6'
       } },
 
+    // 底图-分割线样式
     splitLine: {
       lineStyle: {
         // color: '#e6e6e6'
@@ -197,10 +232,13 @@ var mpvueEcharts = function mpvueEcharts() {return Promise.all(/*! import() | co
   series: [{
     name: '预算 vs 开销（Budget vs spending）',
     type: 'radar',
+    symbol: 'circle', // 拐点的样式，还可以取值'rect','angle'等
+    symbolSize: 10, // 拐点的大小
     data: [
     {
       value: [40, 30, 35, 22, 19],
       name: '预算分配',
+      // 数据-在拐点处显示数值
       label: {
         normal: {
           show: true,
@@ -211,17 +249,21 @@ var mpvueEcharts = function mpvueEcharts() {return Promise.all(/*! import() | co
           } } },
 
 
+      // 数据-区域边框和区域的颜色（以及拐点marker样式）
       itemStyle: {
         normal: {
-          color: '#37aab7' } },
+          color: '#37aab7',
+          borderColor: 'white', // 拐点的描边颜色。[ default: '#000' ]
+          borderWidth: 2 // 拐点的描边宽度，默认不描边。[ default: 0 ]
+        } },
 
-
-
+      // 数据-线条样式
       lineStyle: {
         normal: {
           color: '#37aab7' } },
 
 
+      // 数据-区域样式
       areaStyle: {
         normal: {
           opacity: 0.1,
